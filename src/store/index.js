6c4b4b8f8b47d88886d345ca346e98c4de0cfe4a
Soyoung Kim <sybz0748@gmail.com>
2024-01-11 03:48:52 +0900

    jwt 디코딩, 저장

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,26 @@
 import { createStore } from 'vuex'
+import {jwtDecode} from 'jwt-decode'
 
 export default createStore({
   state: {
     account:{
-      id:0
+      nickname:'',
+      email:''
     }
   },
   getters: {
   },
   mutations: {
-    setAccount(state, payload) {
-      state.account.id = payload
+    login(state, payload) {
+      const claims = jwtDecode(payload)
+      state.account.nickname = claims.nickname
+      state.account.email = claims.email
+      localStorage.setItem('jwt', payload)
+    },
+    logout(state) {
+      state.account.nickname = ''
+      state.account.email = ''
+      localStorage.removeItem('jwt')
     }
   },
   actions: {
