889e4370f1f9d9dedd6cb3651233d78672151596
Soyoung Kim <sybz0748@gmail.com>
2024-01-17 19:47:55 +0900

    github 배포 및 api ip 수정

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/joayo/' // 프로젝트의 GitHub 리포지토리 이름
+    : '/'
 })
