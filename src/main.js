1c1d31d52faa96ef1f88835aaec7bdfbdbd6c27d
Soyoung Kim <sybz0748@gmail.com>
2024-01-21 15:43:20 +0900

    https 적용

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'; // 부트스트랩 아이콘
 
 // axios 설정
 import axios from 'axios'
-axios.defaults.baseURL = 'http://www.joayo.store:8088' // axios 기본 url 설정 [실제 백엔드 API 주소]
+axios.defaults.baseURL = 'https://www.joayo.store:8088' // axios 기본 url 설정 [실제 백엔드 API 주소]
 
 // Axios Interceptor 추가
 import {jwtDecode} from 'jwt-decode'
